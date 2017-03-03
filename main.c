3594d1da8981c129fb8fa90d642278b68f4db563
Anurag Makineni <anurag.makineni@gmail.com>
2017-03-03 18:00:57 -0500

    added printf test

--- main.c
+++ main.c
@@ -7,6 +7,11 @@
 #include "gpio.h"
 #include "dac121c081.h"
 #include "led_conf.h"
+#include "usbcfg.h"
+#include "chprintf.h"
+
+#define usb_lld_connect_bus(usbp)
+#define usb_lld_disconnect_bus(usbp)
 
 int main(void) {
 
@@ -14,10 +19,19 @@ int main(void) {
     chSysInit();
     gpioInit();
     dacInit();
+    
+    sduObjectInit(&SDU1);
+    sduStart(&SDU1, &serusbcfg);
+
+    usbDisconnectBus(serusbcfg.usbp);
+    chThdSleepMilliseconds(150);
+    usbStart(serusbcfg.usbp, &usbcfg);
+    usbConnectBus(serusbcfg.usbp);
 
     while(1)
     {
-       setDAC(LED1_ADDR, 3.3, LED1_VREF);
+       setDAC(LED1_ADDR, 1.0, LED1_VREF);
+       chprintf((BaseSequentialStream *)&SDU1, "%s\n", "test");
        chThdSleepMilliseconds(10);
     }
 }
